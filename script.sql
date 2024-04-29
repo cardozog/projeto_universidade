1d97dee0d03d9bbb49095fe3296a0ea0a77a6c3a
gcandrade <cardozoguilherme18@gmail.com>
2024-04-29 18:44:25 -0300

    remoção da tabela desnecessária turmas_professores, adicionei coluna de unidade pros alunos também

--- script.sql
+++ script.sql
@@ -1,27 +1,31 @@
-create table tb_horarios(
+create table horarios(
 	id_horario serial primary key,
 	dia_semana varchar(20),
 	horario time
 );
 
-create table tb_alunos(
-	matricula_aluno serial primary key,
-	nome_completo varchar(100),
-	email varchar(100),
-	valor_mensalidade decimal(10,2)
-);
-
-create table tb_disciplinas(
-	id_disciplina int primary key, 
-	nome_disciplina varchar(50)
-);
-
-create table tb_unidades_academicas (
+create table unidades_academicas (
 	id_unidade serial primary key,
 	nome_unidade varchar(20)
 );
 
-create table tb_professores(
+create table disciplinas(
+	id_disciplina serial primary key, 
+	nome_disciplina varchar(50)
+);
+
+create table alunos(
+	matricula_aluno serial primary key,
+	nome_completo varchar(100),
+	email varchar(100),
+	valor_mensalidade decimal(10,2),
+	id_unidade int,
+	constraint fk_id_unidade foreign key (id_unidade) references unidades_academicas(id_unidade)
+);
+
+
+
+create table professores(
 	matricula_professor serial primary key,
 	nome_completo varchar(100),
 	email varchar(100) ,
@@ -29,10 +33,10 @@ create table tb_professores(
 	formacao varchar(30),
 	data_inicio date,
 	id_unidade int,
-	constraint fk_id_unidade foreign key (id_unidade) references tb_unidades_academicas(id_unidade)
+	constraint fk_id_unidade foreign key (id_unidade) references unidades_academicas(id_unidade)
 );
 
-create table tb_funcionarios(
+create table funcionarios(
 	matricula_funcionario serial primary key,
 	nome_completo varchar(100),
 	email varchar(100),
@@ -40,25 +44,23 @@ create table tb_funcionarios(
 	funcao varchar(50),
 	data_inicio date,
 	id_unidade int,
-	constraint fk_id_unidade foreign key (id_unidade) references tb_unidades_academicas(id_unidade)
+	constraint fk_id_unidade foreign key (id_unidade) references unidades_academicas(id_unidade)
 );
 
-create table tb_turmas(
+create table turmas(
 	id_turma serial primary key,
 	id_disciplina int,
 	id_horario int,
-	constraint fk_id_horario foreign key(id_horario) references tb_horarios,
-	constraint fk_id_disciplina foreign key(id_disciplina) references tb_disciplinas
+	matricula_professor int,
+	constraint fk_matricula_professor foreign key(matricula_professor) references professores,
+	constraint fk_id_horario foreign key(id_horario) references horarios,
+	constraint fk_id_disciplina foreign key(id_disciplina) references disciplinas
 );
 
-create table tb_alunos_turmas(
+create table alunos_turmas(
 	id_turma int,
 	matricula_aluno int,
 	primary key(id_turma, matricula_aluno)
 );
 
-create table professores_turmas(
-	id_professor int,							
-	id_turma int, 
-	primary key(id_professor,id_turma)					   
-);
+
